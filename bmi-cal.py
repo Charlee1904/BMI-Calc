2b79add11eb4faa225d7cfd1d9441d0c30f6443c
Charlee1904 <charleslee1904@outlook.com>
2022-04-04 22:58:55 -0700

    added the calculations

--- bmi-cal.py
+++ bmi-cal.py
@@ -1,2 +1,9 @@
-heigh = input("Enter your height in m:")
+# Body mass index (BMI) is a value derived from the mass (weight) and height of a person. The BMI is defined as the body mass divided by the square of the body height, and is expressed in units of kg/m2
+
+height = input("Enter your height in m:")
 weight = input("Enter your weight in kg:")
+
+
+bmi = int(weight) / float(height) ** 2
+bmi_int = int(bmi)
+print(bmi_int)
